85cf4758eccfb1dc11bec0dd641906da28b4c7aa
kazu0412 <k.shouhi0412@i.softbank.jp>
2023-10-02 22:47:50 +0900

    add middleware

--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,26 @@
 const express = require("express");
 const router  = express.Router();
 
-// ユーザルート
-router.get("/", (req, res) => {
+// router.use(userLogger); //ミドルウェアは一番上に宣言
+
+// ユーザルート + ミドルウェア適応
+router.get("/", userLogger,(req, res) => {
     res.send("ユーザーです"); 
 })
 
 router.get("/info", (req, res) => {
     res.send("ユーザー情報です"); 
 })
+
+router.get("/:id", (req, res) => {
+    res.send(`${req.params.id}のユーザ情報を取得しました。`); 
+})
+
+// ミドルウェア
+function userLogger(req, res, next){
+    console.log(req.originalUrl);
+    next(); // 次の流れへ
+}
+
 
 module.exports = router;
