c0d382c10b04bd6976df937fc9e0546b4b2a05f8
kazu0412 <k.shouhi0412@i.softbank.jp>
2023-10-05 01:59:45 +0900

    add chat route

--- routes/chat.js
+++ routes/chat.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const chatRouter  = express.Router();
-
-const http = require("http");
 const path = require("path");
-// const io = require("socket.io")(server);
+
+const initChat = (io) => {
+    io.on("connection", (socket) => {
+        console.log('a user connected');
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+    });
+
+    chatRouter.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../", "public", "chat.html"));
+    });
+    
+    return chatRouter;
+}
 
 
-chatRouter.get("/", (req, res) => {
-    // res.send("chat display");
-    res.sendFile(path.join(__dirname, "../","public","chat.html"));
-});
-
-module.exports = chatRouter;
-
+module.exports = initChat;
